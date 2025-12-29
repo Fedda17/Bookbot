356040ff2704dc6069f5eccdf8fbcf65c80bf14c
Fedda17 <fedda.martini@gmail.com>
2025-12-29 19:43:31 +0100

    count chars accounted

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_by_chars, sort_counts
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,5 +6,16 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    count_words(get_book_text("books/frankenstein.txt"))
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    text_string = get_book_text("books/frankenstein.txt")
+    print("----------- Word Count ----------")
+    print(f"Found {count_words(text_string)} total words")
+    print("--------- Character Count -------")
+    char_sorted_list = sort_counts(count_by_chars(text_string))
+    for entry in char_sorted_list:
+        print(f"{entry["char"]}: {entry["num"]}")
+    print("============= END ===============")
+
+
 main()
--- stats.py
+++ stats.py
@@ -1,34 +1,24 @@
 def count_words(content):
-    print(f"Found {len(content.split())} total words")
+    return f"{len(content.split())}"
 
 def count_by_chars(content):
-    chars_counting = {
-        'a': 0,
-        'b': 0,
-        'c': 0,
-        'd': 0,
-        'e': 0,
-        'f': 0,
-        'g': 0,
-        'h': 0, 
-        'i': 0,
-        'j': 0,
-        'k': 0,
-        'l': 0,
-        'm': 0,
-        'n': 0,
-        'o': 0,
-        'p': 0,
-        'q': 0,
-        'r': 0,
-        's': 0,
-        't': 0,
-        'u': 0,
-        'v': 0,
-        'w': 0,
-        'x': 0,
-        'y': 0,
-        'z': 0,
-    }
+    chars_counting = {}
+    for letter in content.lower():
+        if letter in chars_counting:
+            chars_counting[letter] += 1
+        else:
+            chars_counting[letter] = 1
+    return chars_counting
+
+def sort_on(items):
+    return items["num"]
+
+def sort_counts(chars_count_dict):
+    char_count_list = []
+    for char in chars_count_dict:
+        if char.isalpha():
+            char_count_list.append({"char": char, "num": chars_count_dict[char]})
+    char_count_list.sort(reverse=True, key=sort_on)
+    return char_count_list
 
     
